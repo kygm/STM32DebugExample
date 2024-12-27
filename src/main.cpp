ebe94a0c7566e314babf5837d73595249b6e5f49
kygm <rickev20@gmail.com>
2024-12-27 16:39:27 -0600

    Tiny bit of code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,24 @@
 
 #include <Arduino.h>
 
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
 }
 
-// the loop function runs over and over again forever
 int delayMs = 1000;
 
 void loop() {
-    
+
   if(delayMs < 100)
   {
-    delayMs = 800;
+    delayMs = 1000;
   }
 
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(delayMs);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(delayMs);                      // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(delayMs); 
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(delayMs);
 
   delayMs -= 50;
 }
